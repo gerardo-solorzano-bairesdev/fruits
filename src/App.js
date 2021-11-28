ca7f4c12ef31e666b9a89e4be05967aa5aec6c75
Gerardo <gerardo.solorzano@bairesdev.com>
2021-11-28 11:27:38 -0500

    ProductTable OK

--- src/App.js
+++ src/App.js
@@ -2,6 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import ProductCategoryRow from './components/ProductCategoryRow';
 import ProductRow from './components/ProductRow';
+import ProductTable from './components/ProductTable';
+
+const PRODUCTS = [
+  {category: "Fruits", price: "$1", stocked: true, name: "Apple"},
+  {category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit"},
+  {category: "Fruits", price: "$2", stocked: false, name: "Passionfruit"},
+  {category: "Vegetables", price: "$2", stocked: true, name: "Spinach"},
+  {category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin"},
+  {category: "Vegetables", price: "$1", stocked: true, name: "Peas"}
+];
 
 function App() {
   const p1 =  {category: "Fruits", price: "$1", stocked: false, name: "Apple"}
@@ -9,6 +19,7 @@ function App() {
     <div className="App">
       <ProductCategoryRow category="sdfsdf"/>
       <ProductRow product={p1}/>
+      <ProductTable products={PRODUCTS}/>
     </div>
   );
 }
